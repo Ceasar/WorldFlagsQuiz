1ae071a24ffa302a1170b1f29ac442b28a561f61
Ceasar <cbautista2010@gmail.com>
2021-03-06 19:30:50 -0800

    Implement basic world flag quiz app

--- app/src/CountryFlagQuiz.jsx
+++ app/src/CountryFlagQuiz.jsx
@@ -1,5 +1,7 @@
 import _ from 'lodash';
-import React, {useState} from 'react';
+import React, {useCallback, useState} from 'react';
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import {ApolloClient, InMemoryCache, gql, useQuery} from '@apollo/client';
 
 const client = new ApolloClient({
@@ -17,19 +19,85 @@ const LIST_COUNTRIES = gql`
   }
 `;
 
-export default function CountryFlagQuiz() {
-    const [country, setCountry] = useState('US');
-    const {data, loading, error} = useQuery(LIST_COUNTRIES, {client});
-    if (loading || error) {
-        return <p>{error ? error.message : 'Loading...'}</p>;
-    }
-
-    const randomCountry = _.sample(data.countries);
-
-    return (
+// TODO: Use a hash to track button states
+function QuizQuestion(props) {
+  const [chosenAnswer, setChosenAnswer] = useState(null);
+  const [isCorrectAnswer, setIsCorrectAnswer] = useState(null);
+  const onClick = useCallback((event) => {
+    const value = event.target.value;
+    setChosenAnswer(value);
+    setIsCorrectAnswer(props.correctValue === value);
+  }, [props.correctValue]);
+  const onClickNext = useCallback(() => {
+    setChosenAnswer(null);
+    props.onClickNext();
+  }, []);
+  const isChoiceMade = chosenAnswer !== null;
+  return (
+    <div className="quiz">
+      <h2>{props.question}</h2>
+      <div>
+        {props.choices.map(choice => {
+          return (
+            <Button
+              key={choice.key}
+              disabled={isChoiceMade}
+              variant={chosenAnswer === choice.value ? (isCorrectAnswer ? "success" : "danger") : "primary"}
+              value={choice.value}
+              onClick={onClick}
+            >{choice.value}</Button>
+          );
+        })}
+      </div>
+      {isChoiceMade && (
         <div>
-            <h1>Flag Quiz</h1>
-            <h2>{randomCountry.name} {randomCountry.emoji}</h2>
+          <Button onClick={onClickNext} variant="secondary">Next</Button>
         </div>
-    );
+      )}
+    </div>
+  );
+}
+
+function QuizApp({countries, numChoices}) {
+  const [isQuizStarted, setIsQuizStarted] = useState(false);
+  const [countryChoices, setCountryChoices] = useState([]);
+  const [answer, setAnswer] = useState(null);
+  const choices = countryChoices.map(country => ({
+    key: country.code,
+    value: country.name,
+  }));
+  const onClickNext = useCallback(() => {
+    const countryChoices = _.sampleSize(countries, numChoices);
+    setCountryChoices(countryChoices);
+    setAnswer(_.sample(countryChoices));
+  }, []);
+  const onClickStartQuiz = useCallback(() => {
+    setIsQuizStarted(true);
+    onClickNext();
+  });
+  return (isQuizStarted ? (
+    <QuizQuestion
+      question={answer.emoji}
+      choices={choices}
+      correctValue={answer.name}
+      onClickNext={onClickNext}
+    />
+  ) : (
+    <div>
+      <p>Test your knowledge of the flags of the world.</p>
+      <Button onClick={onClickStartQuiz}>Start quiz</Button>
+    </div>
+  ));
+}
+
+export default function CountryFlagQuiz() {
+  const {data, loading, error} = useQuery(LIST_COUNTRIES, {client});
+  return (loading || error) ? (
+    <p>{error ? error.message : 'Loading...'}</p>
+  ) : (
+    <div>
+      <h1>World Flag Quiz</h1>
+      <QuizApp countries={data.countries} numChoices={4} />
+    </div>
+  );
 }
