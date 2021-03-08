9b1a76c0be45513e029c584a80a08e8ed4f91e34
Ceasar <cbautista2010@gmail.com>
2021-03-07 16:01:26 -0800

    Fix warning

--- src/Quiz.jsx
+++ src/Quiz.jsx
@@ -71,7 +71,7 @@ export default function QuizLoader({
         <p>{description}</p>
         <Button onClick={onClickStartQuiz}>Start quiz</Button>
         <div id="github-ribbon">
-          <a href="https://github.com/Ceasar/WorldFlagsQuiz"><img loading="lazy" width="149" height="149" src="https://github.blog/wp-content/uploads/2008/12/forkme_right_white_ffffff.png?resize=149%2C149" class="attachment-full size-full" alt="Fork me on GitHub" data-recalc-dims="1"/></a>
+          <a href="https://github.com/Ceasar/WorldFlagsQuiz"><img loading="lazy" width="149" height="149" src="https://github.blog/wp-content/uploads/2008/12/forkme_right_white_ffffff.png?resize=149%2C149" className="attachment-full size-full" alt="Fork me on GitHub" data-recalc-dims="1"/></a>
         </div>
       </div>
     );
